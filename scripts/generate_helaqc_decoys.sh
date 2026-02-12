0489ed1d425255866fd1221e90cc1488fdd81abe
Jemma Daniel <134346753+JemmaLDaniel@users.noreply.github.com>
2026-02-12 15:07:00 +0000

    chore: add HelaQC dataset preprocessing and evaluation scripts

---
+++ scripts/generate_helaqc_decoys.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.1 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.2 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.3 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.4 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.5 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.6 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.7 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.8 --seed 42
+
+novoboard decoy --spectrum-file ~/Documents/winnow/novoboard/helaqc.mgf --sampling-strategy random --sampling-rate 0.9 --seed 42
